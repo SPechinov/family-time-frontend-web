2a2f2c5b1fcbc8158e5601c822e8275dbcb45547
Sergei Pechinov <spechinov@gmail.com>
2024-12-18 18:17:24 +0100

    Save

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { ErrorMessage } from './shared';
+import { Button, Input, Logo } from './shared';
+import styles from './styles.module.scss';
 
 function App() {
-  console.log('123');
   return (
-    <>
-      <ErrorMessage message="error" />
-    </>
+    <div className={styles.page}>
+      <div className={styles.top}>
+        <Logo />
+        <h1 className={styles.title}>Family Time</h1>
+      </div>
+      <div className={styles.bottom}>
+        <div className={styles.form}>
+          <div className={styles.inputs}>
+            <Input placeholder="Email" />
+            <Input placeholder="Password" />
+          </div>
+          <Button theme="primary">Sign in</Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
