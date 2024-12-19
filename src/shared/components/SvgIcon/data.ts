485457533b5f812610afcc34fe5cfd7cda034280
Sergei Pechinov <spechinov@gmail.com>
2024-12-19 16:08:22 +0100

    Add: i_24_loading.svg

--- src/shared/components/SvgIcon/data.ts
+++ src/shared/components/SvgIcon/data.ts
@@ -2,12 +2,14 @@ import i_24_chevron_down from './icons/i_24_chevron_down.svg';
 import i_24_chevron_right from './icons/i_24_chevron_right.svg';
 import i_24_eye from './icons/i_24_eye.svg';
 import i_24_eye_off from './icons/i_24_eye_off.svg';
+import i_24_loading from './icons/i_24_loading.svg';
 
 export default {
   i_24_chevron_down,
   i_24_chevron_right,
   i_24_eye,
   i_24_eye_off,
+  i_24_loading,
 };
 
 export enum SvgIcons {
@@ -15,4 +17,5 @@ export enum SvgIcons {
   'i_24_chevron_right' = 'i_24_chevron_right',
   'i_24_eye' = 'i_24_eye',
   'i_24_eye_off' = 'i_24_eye_off',
+  'i_24_loading' = 'i_24_loading',
 }
